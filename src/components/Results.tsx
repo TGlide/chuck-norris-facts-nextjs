35524356d10f45aa1c680496ac30d3141d6489a9
tglide <thomglopes@gmail.com>
2020-11-30 23:23:36 +0000

    feat(search): show results

---
+++ src/components/Results.tsx
@@ -0,0 +1,36 @@
+import { Box, BoxProps, Text, VStack } from "@chakra-ui/react";
+import React from "react";
+import { SearchResult } from "../api/Search";
+import theme from "../theme";
+
+type ResultsProps = {
+  results: SearchResult[];
+};
+
+const Results: React.FC<ResultsProps> = ({ results }) => {
+  return (
+    <Box w="100%" mt={4}>
+      <Text fontWeight={600} textAlign="left">
+        {results.length} Results
+      </Text>
+
+      <VStack spacing={4} maxH="50vh" overflowY="scroll" mt={4}>
+        {results.map((result) => {
+          return (
+            <Box
+              boxShadow="2xl"
+              bgColor={theme.colors.green[400]}
+              borderRadius={8}
+              p={6}
+              w="100%"
+            >
+              {result.value}
+            </Box>
+          );
+        })}
+      </VStack>
+    </Box>
+  );
+};
+
+export default Results;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,16 @@ import { useState } from "react";
 import { SearchResult } from "../api/Search";
 import Container from "../components/Container";
 import DarkModeSwitch from "../components/DarkModeSwitch";
+import Results from "../components/Results";
 import Search from "../components/Search";
 
 const Index = () => {
   const [results, setResults] = useState<SearchResult[]>([]);
 
   return (
-    <Container height="100vh">
+    <Container minH="100vh">
       <Search setResults={setResults} />
+      <Results results={results} />
       <DarkModeSwitch />
     </Container>
   );
