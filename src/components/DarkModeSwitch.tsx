e266adacb9771fb95acd4e55c67c1ebede631a11
tglide <thomglopes@gmail.com>
2020-12-03 11:03:34 +0000

    feat: darkmode dynamic icons

--- src/components/DarkModeSwitch.tsx
+++ src/components/DarkModeSwitch.tsx
@@ -1,15 +1,23 @@
-import { MoonIcon } from "@chakra-ui/icons";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 import { Flex, IconButton, useColorMode } from "@chakra-ui/react";
 
 const DarkModeSwitch = () => {
-  const { toggleColorMode } = useColorMode();
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  const isDark = colorMode === "dark";
 
   return (
     <Flex position="fixed" top="1rem" right="1rem" alignItems="center">
       <IconButton
         aria-label="Toggle dark mode"
         onClick={toggleColorMode}
-        icon={<MoonIcon />}
+        icon={
+          isDark ? (
+            <SunIcon w={5} h={5} data-testid="sun" />
+          ) : (
+            <MoonIcon data-testid="moon" />
+          )
+        }
         colorScheme="orange"
       />
     </Flex>
